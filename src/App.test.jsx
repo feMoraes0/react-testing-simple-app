69144b9feb4a815de5b72c2e879d07a84ffccec6
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-06-28 17:29:30 +0100

    first and basic test to validate initial state

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("App contains correct heading", () => {
+// how to check element roles:
+// use logRoles(<App />) to check all roles inside the App component
+// import { logRoles } from "@testing-library/dom"
+// check more details at https://testing-library.com/docs/dom-testing-library/api-debugging/#logroles
+
+test("Button click flow", () => {
   render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).toBeInTheDocument();
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+  expect(buttonElement).toHaveClass("red");
 });
